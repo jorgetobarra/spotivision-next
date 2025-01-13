5c88f13d51f3eb0d75ab149f2c49d655d3e17cf0
Jorge Tobarra <jtobarra@theknotww.com>
2025-01-13 02:02:40 +0100

    fix screen background

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   }, [router, token]);
 
   return (
-    <div className="flex h-screen flex-col bg-bg dark:bg-darkBg md:flex-row md:overflow-hidden">
+    <div className="flex min-h-screen flex-col bg-bg dark:bg-darkBg md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
